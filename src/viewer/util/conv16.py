bce425677a6a01aaf09536f839db387ab939e138
Athapan <aarayas@microsoft.com>
2018-07-19 16:09:32 -0700

    add convert 16 bits -> 8 bits PNG

---
+++ src/viewer/util/conv16.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+import os
+import pathlib
+
+#define the path
+inputDir = 'G:\\'
+inputPath = pathlib.Path(inputDir)
+outputDir = 'E:\\noaa\\conv-thermal'
+
+for currentFile in inputPath.glob("*.png"):
+    currentFilename = str(currentFile.name)
+    inputFile = os.path.join(inputDir, currentFilename)
+    img = cv2.imread(inputFile, -cv2.IMREAD_ANYDEPTH).astype(np.uint8)
+    outputFile = os.path.join(outputDir, currentFilename)
+    cv2.imwrite(outputFile, img)
