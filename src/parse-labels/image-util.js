89bde45b9b73a0c3cde2dd15585b5646397084c9
Peter Baer <pbaer@microsoft.com>
2018-07-23 17:29:16 -0700

    Add code for prepping hotspot crop images

--- src/parse-labels/image-util.js
+++ src/parse-labels/image-util.js
@@ -2,15 +2,17 @@ let fs = require('fs');
 let path = require('path');
 
 const getImageMap = (dirs, type) => {
-    let typeSuffix = 'COLOR-8-BIT.JPG';
+    let typeSubstr = 'COLOR-8-BIT.JPG';
     if (type == 'thermal') {
-        typeSuffix = 'THERM-16BIT.PNG';
+        typeSubstr = 'THERM-16BIT.PNG';
+    } else if (type == 'hotspot') {
+        typeSubstr = '_HOTSPOT_';
     }
     let map = new Map();
     for (let dir of dirs) {
         let files = fs.readdirSync(dir);
         for (let file of files) {
-            if (file.endsWith(typeSuffix)) {
+            if (file.indexOf(typeSubstr) >= 0) {
                 map.set(file, path.join(dir, file));
             }
         }
