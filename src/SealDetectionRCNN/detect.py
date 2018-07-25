5f49f3470a46cbb6192890cdd350c2c194aadd30
Matt Kovacs-Deak <matekd@users.noreply.github.com>
2018-07-25 14:16:34 -0700

    added code for scanning input directory for imgs and running bbox detection over them

---
+++ src/SealDetectionRCNN/detect.py
@@ -0,0 +1,35 @@
+import sys
+import os
+import glob
+import argparse
+import pandas as pd 
+from api import PlasticDetector 
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Given a directory path, scans thermal images and populates a csv file with bounding boxes of predicted seals')
+    parser.add_argument('sourcePath',
+                        help='Relative path to the input directory.')
+    parsed_args = parser.parse_args(sys.argv[1:])
+    return parsed_args
+
+if __name__ == '__main__':
+    args = parse_args()
+    sourcePath = os.path.dirname(os.path.realpath(__file__)) + '/' + args.sourcePath
+    filePattern = "*.png"
+
+    if not os.path.exists(sourcePath):
+        sys.exit("Given source path does not exist")
+    
+    img_paths = glob.glob(sourcePath + '/' + filePattern)
+
+    print("Image files scanned.")
+
+    det = PlasticDetector(
+        'checkpoints/fasterrcnn_07251814_0.877517673670313', True)
+
+    print('Model loaded.')
+
+    bboxes = pd.DataFrame(img_paths.map(lambda img_paths: pd.Series({'img_name': os.path.basename(img_path), 'bboxes': det.predict_bboxes(img_path)})))
+    print(bboxes)
