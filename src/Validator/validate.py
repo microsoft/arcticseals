e299148c3601cadd32cdfa7de224984fa1b8b86a
May Zhu <mayzhu@microsoft.com>
2018-07-25 02:14:03 -0700

    Update validator.

--- src/Validator/validate.py
+++ src/Validator/validate.py
@@ -13,11 +13,17 @@ Note: outputExample and validationWithoutAnomalies are identical right now
 
 # metrics:
 falsePositives = 0
+falseNegatives = 0
 hotSpotDistances = []
 registrationDistances = []
 classificationTrue = 0
 classificationFalse = 0
 expectedTruePositives = 0
+hotspotLocationTrue = 0
+registrationTrue = 0
+
+def getKey(item):
+    return item[5] + item[7]
     
 with open('validationWithoutAnomalies.csv', 'r') as answers:
     # col 5 and 6 = hotspot
@@ -29,7 +35,7 @@ with open('validationWithoutAnomalies.csv', 'r') as answers:
     expectedDict = {}
 
     # fill the dictionary with the expected information
-    for i in range(len(expectedList)): 
+    for i in range(1, len(expectedList)): 
         # assume thermal phone name is id and is in 3rd (index 2) column
         photoID = expectedList[i][2]
         # remove col-name row 
@@ -42,50 +48,99 @@ with open('validationWithoutAnomalies.csv', 'r') as answers:
     with open('outputExample.csv', 'r') as results:
         res = csv.reader(results)
         resList1 = list(res)
+        # filter out anomalies
         resList = list(filter(lambda row: row[11] != "Anomaly", resList1))
-        # todo: filter out anomalies (skip first row of CSV ** asuming there is a title row **)
-        for j in range(1, len(resList)): 
-            resultRow = resList[j]
-            photoID2 = resultRow[2]
-            # if photo not in results dictionary that means there were no animals in the entire photo so throw FP
-            if photoID2 not in expectedDict:
+        resDict = {}
+
+        # Start from the seconde row to exlude the title/col-name row
+        for j in range(1, len(resList)):
+            resPhotoId = resList[j][2]
+            if resPhotoId in resDict:
+                resDict[resPhotoId].append(j)
+            else:
+                resDict[resPhotoId] = [j]
+
+        # Each image may have several hot spots identfied. 
+        # Assuming the spacial distribution pattern of hotspots on classifcation results and ground true are the same, sort the rows
+        # on its x_positon, and macth the spots.
+        # TODO: need to consider method to macth the spots when number of rows are different in result and ground truth
+        for key in resDict:
+            # if photo not in expected dictionary that means there were no animals in the entire photo so throw FalsePositive
+            if key not in expectedDict:
                 falsePositives+=1
             else:
-                rows = expectedDict.get(photoID2)
-                matchedWithExpected = False
-                for i in rows:
-                    # 1 is expected values; 2 is ouput values
-                    row = expectedList[i]
-                    x1 = int(row[5])
-                    y1 = int(row[6])
-                    x2 = int(resultRow[5])
-                    y2 = int(resultRow[6])
+                resRows = resDict.get(key)
+                expectedRows = expectedDict.get(key)
+            # expectedRows.sort(key=lambda row: int(row[5]) + int(row[7])
+
+                lengthDiff = len(resRows) - len(expectedRows)
+                minLength = min(len(resRows), len(expectedRows))
+
+
+                if (lengthDiff >= 0): 
+                    # TODO: updated caculation method when ground truth include "Anormaly"
+                    falsePositives+=lengthDiff
+                else:
+                    falseNegatives+=lengthDiff
+
+                # TODO: need to consider method to macth the spots when number of rows are different in result and ground truth
+                resRowsData = []
+                expectedRowsData = []
+                for k in resRows:
+                    resRowsData.append(resList[k])
+
+                if (len(resRowsData) > 1):
+                    resRowsData.sort(key=getKey)
+
+                for l in expectedRows:
+                    expectedRowsData.append(resList[l])
+
+                if (len(expectedRowsData) > 1):
+                    expectedRowsData.sort(key=getKey)
+
+                for m in range(minLength):
+                    resRow = resRowsData[m]
+                    expectedRow = expectedRowsData[m]
+
+                    # 1 is expected values; 2 is ouput values. Both are absolution location on the image
+                    x1 = int(expectedRow[5])
+                    y1 = int(expectedRow[6])
+                    x2 = int(resRow[5])
+                    y2 = int(resRow[6])
                     dist = math.hypot(x2 - x1, y2 - y1)
-                    # randomly chose 5 but 10 is too big because made expected set find false positives in itself
-                    if dist < 5:
-                        # check bounding boxes
-                        midX1 = (int(row[7]) + int(row[9])) / 2
-                        midY1 = (int(row[8]) + int(row[10])) / 2
-                        midX2 = (int(resultRow[7]) + int(resultRow[9])) / 2
-                        midY2 = (int(resultRow[8]) + int(resultRow[10])) / 2
-                        dist2 = math.hypot(midX2 - midX1, midY2 - midY1)
-                        hotSpotDistances.append(dist)
-                        registrationDistances.append(dist2)
-                        matchedWithExpected = True
-                        # classification accuracy
-                        if (row[12] == resultRow[12]):
-                            classificationTrue += 1 
-                        else:
-                            classificationFalse += 1
-                if (matchedWithExpected == False):
-                    falsePositives+=1
-                
+
+                    # Check bounding box
+                    midX1 = (int(expectedRow[7]) + int(expectedRow[9])) / 2
+                    midY1 = (int(expectedRow[8]) + int(expectedRow[10])) / 2
+                    midX2 = (int(resRow[7]) + int(resRow[9])) / 2
+                    midY2 = (int(resRow[8]) + int(resRow[10])) / 2
+                    dist2 = math.hypot(midX2 - midX1, midY2 - midY1)
+                    hotSpotDistances.append(dist)
+                    # The average size of seal on thermal image is about 2 by 5 pixels. Assume hot spot detedted within 
+                    # the size of a seal is a success detection.
+                    if (dist < 3):
+                        hotspotLocationTrue+=1
+
+                    registrationDistances.append(dist2)
+                    # The average size of seal on color image is about 20 by 70 pixels. Assume an offset at the sale of 
+                    # half the size of a seal won't affect the final result
+                    if (dist2 < 20):
+                        registrationTrue+=1
+                    
+                    # classification accuracy
+                    if (expectedRow[12] == resRow[12]):
+                        classificationTrue += 1 
+                    else:
+                        classificationFalse += 1
 
     print("Percent of hot spots found: " + str((classificationTrue + classificationFalse) / expectedTruePositives))
     print("There were " + str(falsePositives) + " false positives.")
+    print("There were " + str(falseNegatives) + " false negatives.")
     if ((classificationTrue + classificationFalse) > 0):
         print("There was a classification accuracy of " + str((classificationTrue / (classificationTrue + classificationFalse)) * 100) + " percent.")
     if (len(hotSpotDistances) > 0):
-        print("There was an average hot spot distance of " + str(sum(hotSpotDistances) / len(hotSpotDistances)))
+        print("There was an average hot spot distance of " + str(sum(hotSpotDistances) / len(hotSpotDistances)))        
+        print("There was an hot spot location detection accuracy of " + str(hotspotLocationTrue / len(hotSpotDistances)*100)  + " percent.")
     if (len(registrationDistances) > 0):
         print("There was an average registration distance of " + str(sum(registrationDistances) / len(registrationDistances)))
+        print("There was an registration accuracy of " + str(classificationTrue / len(registrationDistances)*100) + " percent.")
