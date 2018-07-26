5ff320ebc20908db217d0902afe4269803a12692
Athapan <aarayas@microsoft.com>
2018-07-25 17:16:43 -0700

    minor fixup for Crop Images

--- src/viewer/util/conv16.py
+++ src/viewer/util/conv16.py
@@ -11,6 +11,7 @@ outputDir = 'E:\\noaa\\conv-thermal'
 for currentFile in inputPath.glob("*.png"):
     currentFilename = str(currentFile.name)
     inputFile = os.path.join(inputDir, currentFilename)
-    img = cv2.imread(inputFile, -cv2.IMREAD_ANYDEPTH).astype(np.uint8)
+    #img = cv2.imread(inputFile, -cv2.IMREAD_ANYDEPTH).astype(np.uint8)
+    img = cv2.imread(inputFile, cv2.IMREAD_LOAD_GDAL).astype(np.uint8)
     outputFile = os.path.join(outputDir, currentFilename)
     cv2.imwrite(outputFile, img)
