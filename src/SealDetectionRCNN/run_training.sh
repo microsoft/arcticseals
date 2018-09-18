a3ebfdf825659b460f1e285da78c6efd45955c6b
Ubuntu <loris@lorisvm.mw1gg3hcns4uhnmukksightp5f.bx.internal.cloudapp.net>
2018-09-18 17:06:05 +0000

    Preparing for IR training

--- src/SealDetectionRCNN/run_training.sh
+++ src/SealDetectionRCNN/run_training.sh
@@ -3,10 +3,10 @@ python train.py train \
 --plot-every=100 \
 --caffe-pretrain  \
 --dataset='vott' \
---train_image_dir='/datadrive/train/' \
---val_image_dir='/datadrive/test/' \
+--train_image_dir='/data/seals/seal_data_vott/sealdata_large_IR_N/train/' \
+--val_image_dir='/data/seals/seal_data_vott/sealdata_large_IR_N/test/' \
 --num_epochs=200 \
 --epoch_decay=150 \
---plot_every=20 \
---max_size=5000
+--plot_every=20 
+# --max_size=5000
 #--load_path='checkpoints/fasterrcnn_07101509'
