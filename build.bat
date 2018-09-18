0804bfeac281cb2e1225f9d34b85edf75bae92b4
Dan Morris <dan@microsoft.com>
2018-09-18 06:51:25 -0700

    Added seals_dataset.py (recovered), started moving some old stuff to "archive"

--- build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-@rem This script ensures that dependent packages are installed, and any build steps (if applicable) are performed.
-@rem Assumes certain prerequisites are installed as described in the root README.md. Please add to this as needed.
-
-cd %~dp0/src/parse-labels
-npm install
