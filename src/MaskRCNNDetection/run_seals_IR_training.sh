1063582423b03a77e8aa1f79765eac888d8a8e16
Marcel Simon <a-msimon@microsoft.com>
2019-02-20 07:20:00 +0000

    MaskRCNN code: increased training steps to compensate for the amount of training data

--- src/MaskRCNNDetection/run_seals_IR_training.sh
+++ src/MaskRCNNDetection/run_seals_IR_training.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH=$(pwd)
-python tools/train_net.py --config-file "configs/e2e_faster_rcnn_X_101_32x8d_FPN_1x.yaml" DATASETS.TRAIN \(\'vott_seals_large_IR_N_train\',\) DATASETS.TEST \(\"vott_seals_large_IRN_test\",\) SOLVER.IMS_PER_BATCH 1 SOLVER.BASE_LR 0.00125 TEST.IMS_PER_BATCH 1 SOLVER.STEPS \(1200,1600\) SOLVER.MAX_ITER 2000
+python tools/train_net.py --config-file "configs/e2e_faster_rcnn_X_101_32x8d_FPN_1x.yaml" DATASETS.TRAIN \(\'vott_seals_large_IR_N_train\',\) DATASETS.TEST \(\"vott_seals_large_IR_N_test\",\) SOLVER.IMS_PER_BATCH 1 SOLVER.BASE_LR 0.00125 TEST.IMS_PER_BATCH 1 SOLVER.STEPS \(16000,18000\) SOLVER.MAX_ITER 20000
